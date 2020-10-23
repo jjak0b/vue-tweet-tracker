897c74f5a7e872ce72a335c8bd33b4497704ca75
Luca Bennati <lucabennati2k16@gmail.com>
2020-10-23 17:15:11 +0200

    migliorata codifica a json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const Twitter = require('twitter')  //twitter api
 const nconf = require('nconf')      //configuration file
 const fs = require('fs')            //filesystem
-const mail = require('nodemailer')  //mail
 
 const config = nconf.file('../keys.json')   //file con le key per le twitter api
 
@@ -11,35 +10,13 @@ const client = new Twitter({    //configurazione client di twitter
     bearer_token: config.get("Bearer_token")
 });
 
-const transporter = mail.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'twittertracker202014@gmail.com',
-        pass: 'tt202014'
-    }
-})
-
-var mailOptions = {
-    from: 'twittertracker202014@gmail.com',
-    to: 'lucabennati2k16@gmail.com',
-    subject: 'test mail node js',
-    text: 'speriamo vada'
-}
-
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-});
-       
-/*
-client.get('search/tweets', {q: "from:LucaBennati2" }, (error,tweets,res) =>{
-    console.log(tweets)
-    let data = JSON.stringify(tweets);
+//in q metto i parametri di ricerca, tweet_mode: 'extended' serve per visualizzare l'intero tweet nel campo text
+client.get('search/tweets', {q: '#ingsw2020', tweet_mode: 'extended'}, (error,tweets,res) =>{
+    //console.log(tweets)
+    let data = JSON.stringify(tweets, ""," ");
+    
     //salvo i dati su file
     fs.writeFile('../tweets.json', data, (err) =>{
         if (err) throw err;
     })
-}) */
+})
