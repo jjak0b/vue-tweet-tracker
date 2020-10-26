ec065535698df410ea185f1ac1924006afe10439
Luca Bennati <lucabennati2k16@gmail.com>
2020-10-26 18:13:14 +0100

    backend/twitterapi 2.0 Passaggio da api 1.1 a 2.0

--- src/index.js
+++ src/index.js
@@ -1,44 +1,51 @@
-const Twitter = require('twitter')  //twitter api
 const nconf = require('nconf')      //configuration file
 const fs = require('fs')            //filesystem
 const config = nconf.file('../keys.json')   //file con le key per le twitter api
-
-const client = new Twitter({    //configurazione client di twitter
-    access_token_key: config.get("Api_key"),
-    access_token_secret: config.get("Api_Secret_key"),
-    bearer_token: config.get("Bearer_token")
-});
-
-//in q metto i parametri di ricerca, tweet_mode: 'extended' serve per visualizzare l'intero tweet nel campo text
-client.get('search/tweets', {q: '#ingsw2020', tweet_mode: 'extended'}, (error,tweets,res) =>{
-    //console.log(tweets)
-    let data = JSON.stringify(tweets, null,2);  //in data sono presenti i tweet in formato json
-    let json = JSON.parse(data) //trasformo il formato json in un oggetto
-    let array = []      //array che contiene le informazioni utili di un tweet
-    let jsonfromarray   //array in json
-    for(let i = 0; i < json.statuses.length; i++){  //scorro il file json
-        if(json.statuses[i].place != null){         //se è presente la geolocalizzazione la includo, altrimenti no
-            array.push(json.statuses[i].created_at, json.statuses[i].full_text,  json.statuses[i].user.name, json.statuses[i].place.full_name, json.statuses[i].place.country)
-        }
-        else {
-            array.push(json.statuses[i].created_at, json.statuses[i].full_text, json.statuses[i].user.name)
-        }
-        jsonfromarray = JSON.stringify(array, null, 2)  //trasformo l'array con i tweet in formato json
-    }
-
-    console.log(jsonfromarray)
-
-    //salvo i dati su file
-    /*fs.writeFile('../tweets.json', data, (err) =>{
-        if (err) throw err;
-    })
-    fs.readFile('../tweets.json', (err, data) => {
-        let json = JSON.parse(data)
-        for(let i = 0; json.statuses.length; i++){
-            console.log(json.statuses[i].created_at)
-        }
-        })*/
-    })
+const needle = require('needle')
 
 //TO-DO
-//inserire i dati raccolti su database
+//usare api v2.0
+
+const bearer = config.get('Bearer_token')   //bearer token
+const endpointUrl = 'https://api.twitter.com/2/tweets/search/recent'
+
+async function getRequest(){    //funzione per richiedere i dati da twitter
+    const params = {            //parametri della richiesta
+        'query': '#ingsw2020',
+        'tweet.fields': 'author_id,created_at,id,possibly_sensitive,text,geo',
+        'place.fields': 'full_name,geo,name,place_type',
+        'user.fields': 'name,created_at'
+    }
+    const res = await needle('get', endpointUrl, params, { headers: { "authorization": `Bearer ${bearer}` }})   //richiesta
+
+    if(res.body){
+        return res.body
+    }
+    else{
+        throw new Error ('Richiesta non riuscita')
+    }
+}
+
+async function saveData(data){  //funzione per salvare i dati ottenuti in un file json
+    let jsondata = JSON.stringify(data, null, 4)
+    fs.writeFileSync('../tweets.json', jsondata, (err) =>{
+        if (err) throw err;
+    })
+    console.log('file saved')
+}
+
+(async () => {
+
+    try {
+        // Make request
+        const response = await getRequest();
+        console.log(response)
+        await saveData(response)
+
+    } catch(e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+  })();
+
