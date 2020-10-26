d2a20ca2f3fa60067dee7b8814d16ad751e7a508
Martina Accorsi <martina.accorsi4@studio.unibo.it>
2020-10-26 02:36:28 +0100

    Bozza home page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
   <v-app>
-    <v-main>
+    <div id="home_back">
+     <v-container>
+      <v-row id="first_row">
+        <v-img
+            src="./assets/img/titolo1.png"
+            contain
+        >
+        </v-img>
+      </v-row>
 
-    </v-main>
+      <v-spacer> </v-spacer>
+
+      <v-row id="second_row"
+          align="center"
+          justify="space-around"
+      >
+        <v-btn
+            outlined
+            color="indigo"
+        >
+          Crea Evento
+        </v-btn>
+
+        <v-btn
+            outlined
+            color="indigo"
+        >
+          Nuovo Campionamento
+        </v-btn>
+
+        <v-btn
+            outlined
+            color="indigo"
+        >
+          Lista Campionamenti
+        </v-btn>
+
+      </v-row>
+     </v-container>
+    </div>
   </v-app>
 </template>
 
 <script>
-// import Component from './components/Component';
 
 export default {
   name: 'App',
-
-  components: {
-  },
 
   data: () => ({
   }),
