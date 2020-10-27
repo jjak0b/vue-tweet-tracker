a660137fb4931949ab7a704886c3d2475288146c
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-10-27 10:26:16 +0100

    [Backend/API] TODO: Fix oAuth 1.0

--- backend/api/getRecent.js
+++ backend/api/getRecent.js
@@ -2,42 +2,39 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const needle = require('needle');
-// const crypto = require('crypto');
+const crypto = require('crypto');
 
 const API_searchRecent = 'https://api.twitter.com/2/tweets/search/recent';
 let auto20 = {
     "authorization": `Bearer ${ process.env.TWITTER_API_BEARER_TOKEN }`
 };
 
-/*
 // va verificare
-https://developer.twitter.com/en/docs/authentication/oauth-1-0a/creating-a-signature
+// https://developer.twitter.com/en/docs/authentication/oauth-1-0a/creating-a-signature
 
-let now = Date.now();
-let nonce = now;
-let signature_method = "HMAC-SHA1";
+const signature_method = "SHA1"; // HMAC-SHA1
 let signingKey = `${encodeURIComponent(process.env.TWITTER_API_CONSUMER_SECRET)}&${encodeURIComponent(process.env.TWITTER_API_ACCESS_TOKEN_SECRET)}`;
-let signature = crypto.createHash(signature_method, signingKey);
 
-var hmac = crypto.createHmac(signature_method, signingKey).update(signingKey).digest('binary');;
+var signature = crypto.createHmac(signature_method, signingKey).update(signingKey).digest('base64');
 
 let auto10a = {
     "authorization" : "OAuth " +
-        `oauth_consumer_key="${process.env.TWITTER_API_CONSUMER_KEY}"` + ",\n" +
-        `oauth_token="${process.env.TWITTER_API_ACCESS_TOKEN}"` + ",\n" +
-        `oauth_signature="${signature}"` + ",\n" +
-        `oauth_signature_method="${ signature_method }"` + ",\n" +
-        `oauth_timestamp="${now}"` + ",\n" +
-        `oauth_nonce="${nonce}"` + ",\n" +
-        `oauth_version="${"1.0"}"` + ",\n";
-
+        `oauth_consumer_key="${process.env.TWITTER_API_CONSUMER_KEY}", ` +
+        `oauth_token="${process.env.TWITTER_API_ACCESS_TOKEN}", ` +
+        `oauth_signature="${signature}", ` +
+        `oauth_signature_method="${ signature_method }", ` +
+        `oauth_version="${"1.0"}, `
 }
-*/
 
-let auth = auto20;
+let auth = auto10a;
 
 
 router.get( "/:query", (req, res) => {
+    let now = Date.now();
+    let nonce = now;
+    auto10a.authorization +=    `oauth_nonce="${nonce}", ` +
+                                `oauth_timestamp="${now}"`;
+    console.log( auto10a );
     const params = {            //parametri della richiesta
         query: req.params.query,
         max_results: 100,
