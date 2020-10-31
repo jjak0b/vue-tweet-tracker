d560f02163134c3ce672e5b529e2db870ec41864
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-10-31 22:07:21 +0100

    [Backend/filter] Fix filtering

--- backend/js/filterConverter.js
+++ backend/js/filterConverter.js
@@ -22,7 +22,7 @@ function convertFilter(filter){
             }
 
             if( filter.words.language ) {
-                query += ` lang:${ convertLang(filter.words.language) }`;
+                query += ` lang:${ filter.words.language }`;
             }
         }
 
@@ -57,171 +57,11 @@ function convertFilter(filter){
             if( filter.filters.linksValue ) {
                 query += ` url:${filter.filters.linksValue}`;
             }
-            if( filter.filters.replies ) {
-                query += ` filter:links`;
-            }
-        }
-        if( filter.engagement) {
-            if( filter.engagement ) {
-                query += ` filter:links`;
-            }
         }
 
         query = query.trim();
     }
     return query;
-}
-
-function convertLang(lang){
-
-    if (lang == "Arabic")
-        return "ar"
-    else if (lang == "Bulgarian")
-        return "bg"
-    else if (lang == "Czech")
-        return "cs"
-    else if (lang == "Danish")
-        return "da"
-    else if (lang == "Dutch")
-        return "nl"
-    else if (lang == "English")
-        return "en"
-    else if (lang == "Finnish")
-        return "fi"
-    else if (lang == "French")
-        return "fr"
-    else if (lang == "German")
-        return "de"
-    else if (lang == "Greek")
-        return "el"
-    else if (lang == "Gujarati")
-        return "gu"
-    else if (lang == "Hebrew")
-        return "iw"
-    else if (lang == "Hindi")
-        return "hi"
-    else if (lang == "Hungarian")
-        return "hu"
-    else if ( lang == "Indonesian")
-        return "in"
-    else if ( lang == "Italian")
-        return "it"
-    else if ( lang == "Japanese")
-        return "ja"
-    else if ( lang == "Kannada")
-        return "kn"
-    else if ( lang == "Korean")
-        return "ko"
-    else if ( lang == "Marathi")
-        return "mr"
-    else if ( lang == "Norwegian")
-        return "no"
-    else if ( lang == "Persian")
-        return "fa"
-    else if ( lang == "Polish")
-        return "pi"
-    else if ( lang == "Romanian")
-        return "ro"
-    else if ( lang == "Portuguese")
-        return "pt"
-    else if ( lang == "Russian")
-        return "ru"
-    else if ( lang == "Serbian")
-        return "sr"
-    else if ( lang == "Traditional Chinese")
-        return "zh"
-    else if ( lang == "Slovak")
-        return "sk"
-    else if ( lang == "Swedish")
-        return "sw"
-    else if ( lang == "Tamil")
-        return "ta"
-    else if ( lang == "Thai")
-        return "th"
-    else if ( lang == "Turkish")
-        return "tr"
-    else if ( lang == "Ukrainian")
-        return "uk"
-    else if ( lang == "Urdu")
-        return "ur"
-    else if ( lang == "Vietnamese")
-        return "vi"
-
-    /* non funzionante, ma lo tengo per sicurezza
-    switch(lang){
-        else if ( lang == "Arabic":
-            return "ar"
-        else if ( lang == "Bulgarian":
-            return "bg"
-        else if ( lang == "Czech":
-            return "cs"
-        else if ( lang == "Danish":
-            return "da"
-        else if ( lang == "Dutch":
-            return "nl"
-        else if ( lang == "English":
-            return "en"
-        else if ( lang == "Finnish":
-            return "fi"
-        else if ( lang == "French":
-            return "fr"
-        else if ( lang == "German":
-            return "de"
-        else if ( lang == "Greek":
-            return "el"
-        else if ( lang == "Gujarati":
-            return "gu"
-        else if ( lang == "Hebrew":
-            return "iw"
-        else if ( lang == "Hindi":
-            return "hi"
-        else if ( lang == "Hungarian":
-            return "hu"
-        else if ( lang == "Indonesian":
-            return "in"
-        else if ( lang == "Italian":
-            return "it"
-        else if ( lang == "Japanese":
-            return "ja"
-        else if ( lang == "Kannada":
-            return "kn"
-        else if ( lang == "Korean":
-            return "ko"
-        else if ( lang == "Marathi":
-            return "mr"
-        else if ( lang == "Norwegian":
-            return "no"
-        else if ( lang == "Persian":
-            return "fa"
-        else if ( lang == "Polish":
-            return "pi"
-        else if ( lang == "Romanian":
-            return "ro"
-        else if ( lang == "Portuguese":
-            return "pt"
-        else if ( lang == "Russian":
-            return "ru"
-        else if ( lang == "Serbian":
-            return "sr"
-        else if ( lang == "Traditional Chinese":
-            return "zh"
-        else if ( lang == "Slovak":
-            return "sk"
-        else if ( lang == "Swedish":
-            return "sw"
-        else if ( lang == "Tamil":
-            return "ta"
-        else if ( lang == "Thai":
-            return "th"
-        else if ( lang == "Turkish":
-            return "tr"
-        else if ( lang == "Ukrainian":
-            return "uk"
-        else if ( lang == "Urdu":
-            return "ur"
-        else if ( lang == "Vietnamese":
-            return "vi"
-    }*/
 }
 
 module.exports.convertfilter = convertFilter;
