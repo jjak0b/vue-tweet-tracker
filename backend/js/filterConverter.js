3e8d4c582f0961436a516e26220eaf7c41fe98ba
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-10-31 22:51:03 +0100

    [Backend] Fix filter not recognize empty arrays

--- backend/js/filterConverter.js
+++ backend/js/filterConverter.js
@@ -3,39 +3,39 @@ function convertFilter(filter){
     let query = "";
     if( filter ) {
         if( filter.words ) {
-            if( filter.words.all ) {
+            if( filter.words.all && filter.words.all.length > 0 ) {
                 query += ` ${ filter.words.all.join( " " )}`;
             }
-            if( filter.words.exact ) {
+            if( filter.words.exact && filter.words.exact.length > 0 ) {
                 query += ` "${filter.words.exact.join( " " )}"`;
             }
-            if( filter.words.any ) {
+            if( filter.words.any && filter.words.any.length > 0 ) {
                 query += ` ${ filter.words.any.join( " OR " )}`;
             }
-            if( filter.words.none ) {
+            if( filter.words.none && filter.words.none.length > 0 ) {
                 query += ` -${filter.words.none.join( " -" )}`;
             }
 
-            if( filter.words.hashtags ) {
+            if( filter.words.hashtags && filter.words.hashtags.length > 0 ) {
                 filter.words.hashtags.forEach( (item, key, array) => array[ key ] = item.replace(/#/g, "") )
                 query += ` #${filter.words.hashtags.join( " #" )}`;
             }
 
-            if( filter.words.language ) {
+            if( filter.words.language && filter.words.language.length > 0 ) {
                 query += ` lang:${ filter.words.language }`;
             }
         }
 
         if( filter.accounts ) {
-            if( filter.accounts.from ) {
+            if( filter.accounts.from && filter.accounts.from.length > 0 ) {
                 filter.accounts.from.forEach( (item, key, array) => array[ key ] = item.replace(/@/g, "") )
                 query += ` from:${filter.accounts.from.join( " from:")}`;
             }
-            if( filter.accounts.to ) {
+            if( filter.accounts.to && filter.accounts.to.length > 0 ) {
                 filter.accounts.to.forEach( (item, key, array) => array[ key ] = item.replace(/@/g, "") )
                 query += ` to:${filter.accounts.to.join( " to:")}`;
             }
-            if( filter.accounts.mentioning ) {
+            if( filter.accounts.mentioning && filter.accounts.mentioning > 0 ) {
                 filter.accounts.mentioning.forEach( (item, key, array) => array[ key ] = item.replace(/@/g, "") )
                 query += ` @${filter.accounts.mentioning.join( " @")}`;
             }
