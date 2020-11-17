628108ba8a20a9aaaaca050e2a47684c5c57095b
Martina Accorsi <martina.accorsi4@studio.unibo.it>
2020-11-17 17:09:10 +0100

    Gallery iniziata

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,15 +1,38 @@
 <template>
   <v-container>
+    <v-row v-for=" in selectedSample" >
 
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import json from "../../backend/repositories/testMultipleImmagini.json"
+import axios from "axios";
+
 export default {
   name: "Gallery",
   props: {
     selectedSample: String
-  }
+  },
+
+  data: () => ({
+    tweets: json,
+  }),
+
+  watch: {
+    selectedSample: function (newVal) {
+      if ( newVal && newVal.length > 0 ) {
+        axios.get('/api/samples/' + newVal)
+            .then( (response) => {
+              this.tweets = response.data;
+            })
+            .catch( (error) => {
+              console.error("ERROR", error);
+            })
+      }
+    }
+  },
 }
 </script>
 
