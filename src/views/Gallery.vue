4c007d9d4238027a90b9c1de8ad4ea2f189875f3
Martina Accorsi <martina.accorsi4@studio.unibo.it>
2020-11-18 12:28:40 +0100

    Versione stabile della gallery (ancora da implementare graficamente), commit previa prova di ottimizzazione del codice

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,7 +1,14 @@
 <template>
   <v-container>
-    <v-row v-for=" in selectedSample" >
-
+    <v-row v-for="item in places" v-bind:key="item.id">
+      <p>{{item.id}}</p>
+      <v-img v-for="(media,i) in item.media"
+             :key="i"
+             :src="media.url"
+             max-height="200px"
+             max-width="200px"
+      >
+      </v-img>
     </v-row>
   </v-container>
 </template>
@@ -17,10 +24,12 @@ export default {
   },
 
   data: () => ({
-    tweets: json,
-  }),
+      tweets: json,
+      places:[]
+    }),
 
   watch: {
+
     selectedSample: function (newVal) {
       if ( newVal && newVal.length > 0 ) {
         axios.get('/api/samples/' + newVal)
@@ -33,6 +42,55 @@ export default {
       }
     }
   },
+
+  created() {
+    this.setPlaces();
+  },
+
+  methods: {
+    setPlaces:function(){
+      let alreadyAdded = false;
+
+      for (let tweet of this.tweets){
+        if(tweet.media){
+          if(tweet.places.full_name){
+            for(let place of this.places){
+              if(tweet.places.full_name == place.id){
+                for(let media of tweet.media){
+                  place.media.push(media);
+                }
+                alreadyAdded = !alreadyAdded;
+              }
+            }
+            if(!alreadyAdded){
+             this.places.push(
+                  {
+                    id: tweet.places.full_name,
+                    media: tweet.media
+                  }
+              );
+            }
+          }
+          else if(tweet.users.location){
+            for(let place of this.places){
+              if(tweet.users.location == place.id){
+                place.media.push(tweet.media);
+                alreadyAdded = !alreadyAdded;
+              }
+            }
+            if(!alreadyAdded){
+              this.places.push(
+                  {
+                    id: tweet.users.location,
+                    media: tweet.media
+                  }
+              );
+            }
+          }
+        }
+      }
+    }
+  }
 }
 </script>
 
