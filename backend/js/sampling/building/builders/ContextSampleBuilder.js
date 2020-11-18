e154aa3133828508aae87011950d8217ef0bd92a
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-11-18 16:28:44 +0100

    [Backend] Add ContextSampleBuilder class to extend AbstractSampleBuilder to build context type samples

---
+++ backend/js/sampling/building/builders/ContextSampleBuilder.js
@@ -0,0 +1,32 @@
+const AbstractSampleBuilder = require("./AbstractSampleBuilder");
+const SampleDescriptor = require("../parts/SampleDescriptor");
+const ContextFilteringRule = require("../parts/filters/rules/ContextFilteringRule");
+const ContextFilter = require("../parts/filters/ContextFilter");
+const JSONBufferedItemsCollection = require("../../../JSONBufferedItemsCollection");
+const FSResourceStorage = require("../../../FSResourceStorage");
+
+class ContextSampleBuilder extends AbstractSampleBuilder {
+    constructor(path) {
+        super(path);
+    }
+
+    buildDescriptor( rawFilter, location ) {
+        let filter = new ContextFilter( rawFilter );
+
+        let tag = this.getSample().tag;
+
+        let rule = new ContextFilteringRule( tag, null, filter);
+
+        let descriptor = new SampleDescriptor( tag, rule, location );
+        descriptor.setStorage( FSResourceStorage.getInstance() );
+
+        this.getSample().setDescriptor(descriptor);
+    }
+
+    buildCollection( initBuffer, location ) {
+        let collection = new JSONBufferedItemsCollection( location, initBuffer, 128 );
+        this.getSample().setCollection( collection );
+    }
+}
+
+module.exports = ContextSampleBuilder;
