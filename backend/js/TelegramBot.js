cc8fa2fea3b891a7033474a3a54c9a5c39f3a736
S1mplyD <lucabennati2k16@gmail.com>
2020-11-19 17:13:09 +0100

    [backend/TelegramBot] completed some commands commands completed: add,stop and start bot commands

--- backend/js/TelegramBot.js
+++ backend/js/TelegramBot.js
@@ -1,27 +1,28 @@
 const { Telegraf } = require('telegraf');
-const fs = require('fs')
+const fs = require('fs');
 const path = require("path");
+const Markup = require('telegraf/markup');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 let filepath = path.join( global.__basedir, "..", "telegram.json" );
+
 bot.start((ctx) => {
     ctx.reply('Progetto SWE gruppo 14')
 
-    let user ={
-        ids:[{
-            chatid: ctx.message.chat.id
-        }]
-    }
     let json = JSON.parse(fs.readFileSync( filepath ) );
     if(Object.keys(json).length != 0){
-        json['ids'].push({chatid:ctx.message.chat.id})
-        console.log(json)
-        user = JSON.stringify(json,null,4)
-        fs.writeFileSync(filepath, user)
+        for(let i = 0; i < Object.keys(json).length; i++){
+            if(json[i].chatid != ctx.message.chat.id){
+                json.push({chatid: ctx.message.chat.id})
+            }
+        }
+        json = JSON.stringify(json,null,4)
+        fs.writeFileSync(filepath, json)
     }
     else{
-        user = JSON.stringify(user,null,4)
-        fs.writeFileSync(filepath, user)
+        json.push({chatid: ctx.message.chat.id})
+        json = JSON.stringify(json,null,4)
+        fs.writeFileSync(filepath, json)
     }
 
 });
@@ -30,26 +31,62 @@ bot.help(ctx => {
     ctx.reply('Contattare Gruppo 14')
 });
 
+bot.command('add', ctx => {
 
+    let tags = ['terremoto', 'incendio', 'proteste']    //esempio per test, poi si farà una request per ottenere i tag dal server
+    let users = JSON.parse(fs.readFileSync(filepath))
+    let followed = []
+    for(let i = 0; i < users.length; i++){
+        if(users[i].chatid == ctx.message.chat.id){
+            if(users[i].hasOwnProperty('events')){
+                for(let j = 0; j < users[i].events.length; j++)
+                    followed.push(users[i].events[j])
+            }
+        }
+    }
+
+    tags = tags.filter(val => !followed.includes(val))
+    ctx.reply('Events',Markup
+        .keyboard(tags)
+        .oneTime()
+        .resize()
+        .extra()
+    )
+    bot.on('message', ctx =>{
+        let arr = []
+        if(tags.includes(ctx.message.text)){
+            for(let i = 0; i < users.length; i++){
+                if(users[i].chatid == ctx.message.chat.id){
+                    if(users[i].hasOwnProperty('events')){
+                        for(let j = 0; j < users[i].events.length; j++){
+                            arr.push(users[i].events[j])
+                        }
+                        arr.push(ctx.message.text)
+                        users[i].events = arr
+                        fs.writeFileSync(filepath, JSON.stringify(users, null, 4))
+                    }
+                    else{
+                        arr.push(ctx.message.text)
+                        users[i].events = arr
+                        fs.writeFileSync(filepath, JSON.stringify(users, null, 4))
+                    }
+                }
+            }
+        }
+
+    })
+})
 
 bot.command('stop', ctx =>{
+
     let json = JSON.parse(fs.readFileSync(filepath))
-    console.log(json)
     let id = ctx.message.chat.id
-    for(let i = 0; i < json.ids.length; i++){
-        if(id === json.ids[i].chatid){
-            json.ids.splice(i,1)
+    for(let i = 0; i < json.length; i++){
+        if(id === json[i].chatid){
+            delete json[i].events
             fs.writeFileSync(filepath,JSON.stringify(json,null,4))
         }
     }
-});
-
-bot.on('message', (ctx)=>{
-    let json = JSON.parse(fs.readFileSync(filepath))
-    for(let i = 0; i < json.ids.length;i++){
-        bot.telegram.sendMessage(json.ids[i].chatid, 'prova send msg')
-    }
-
 });
 
 bot.launch();
