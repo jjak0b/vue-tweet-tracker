8ab1d2e62073aadf34e065d601cf35eeb148bd82
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-11-20 01:38:03 +0100

    [Backend] Add AbstractSamplingStrategy

---
+++ backend/js/sampling/strategies/AbstractSamplingStrategy.js
@@ -0,0 +1,92 @@
+const SampleDirector = require("../building/directors/SampleDirector");
+
+class AbstractSamplingStrategy {
+    /**
+     *
+     * @param controller {SamplingController}
+     */
+    constructor( controller ) {
+
+        this.controller = controller;
+        /**
+         *
+         * @type {SampleDirector}
+         */
+
+        this.sampleDirector = new SampleDirector();
+    }
+
+
+    /**
+     *
+     * @return {SampleDirector}
+     */
+    getDirector() {
+        return this.sampleDirector;
+    }
+
+    setDirector( /*SampleDirector*/director ) {
+        this.sampleDirector = director;
+    }
+
+    /**
+     *
+     * @return {SamplingController}
+     */
+    getController() {
+        return this.controller;
+    }
+
+    setController( /*SamplingController*/ controller ) {
+        this.controller = controller;
+    }
+
+    /**
+     *
+     * @return {Sample}
+     */
+    create( tag, filter ) {
+        this.sampleDirector.constructSample( tag, filter );
+        return this.sampleDirector.getSample();
+    }
+
+    /**
+     *
+     * @param sample {Sample}
+     */
+    async delete( sample ) {
+        this.sampleDirector.deconstructSample( sample );
+    }
+
+    /**
+     * @return {Promise<void>}
+     */
+    async fetch( ) {
+
+    }
+
+    async start() {
+
+    }
+
+    async stop() {
+
+    }
+
+    /**
+     *
+     * @param sample {Sample}
+     * @return {Promise<void>}
+     */
+    async resume( sample ) {}
+
+    /**
+     *
+     * @param sample {Sample}
+     * @return {Promise<void>}
+     */
+    async pause( sample ) {}
+
+}
+
+module.exports = AbstractSamplingStrategy;
