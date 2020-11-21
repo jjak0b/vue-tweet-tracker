b72e1b7f19446b99c85ad17c7706399762471069
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-11-21 20:21:33 +0100

    [Backend] Setup for Only Geo sampling using api 1.1

---
+++ backend/js/TweetFetchFSAppendResourceStorage.js
@@ -0,0 +1,11 @@
+const FSAppendResourceStorage = require("./FSAppendResourceStorage");
+
+
+class TweetFetchFSAppendResourceStorage extends FSAppendResourceStorage{
+    constructor() {
+        super();
+    }
+
+
+
+}
