1339dbca24fb51d257c207f9f94799272fb2c217
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-12-02 17:14:07 +0100

    [Backend] SocialContentProvider as Facade to publish a preset of contents Now published post contains a status text and a generated word cloud

---
+++ backend/js/posting/SocialContentProvider.js
@@ -0,0 +1,51 @@
+const TwitterSocialPostingService = require("./services/TwitterSocialPostingService");
+const SocialPostingHandler = require("./handlers/SocialPostingHandler");
+const WordCloudImageDirector = require("./directors/WordCloudImageDirector");
+const WordCloudImageBuilder = require("./builders/WordCloudImageBuilder");
+
+class SocialContentProvider {
+    constructor() {
+
+        this.startHandler = new SocialPostingHandler(
+            new TwitterSocialPostingService()
+        )
+
+        // add new services using
+        // this.startHandler.setNextHandler(
+        //     new SocialPostingHandler(
+        //         new ISocialPostingService()
+        //     )
+        // )
+
+    }
+
+    /**
+     *
+     * @param status
+     * @param wordMap {WordMap}
+     * @return {Promise<*[] | Number>}
+     */
+    async publishPost( status, wordMap ) {
+        let wordCloudBuilder = new WordCloudImageBuilder();
+        let wordCloudDirector = new WordCloudImageDirector( wordCloudBuilder );
+
+        try {
+            await wordCloudDirector.construct(wordMap, 1280, 720);
+
+            let imageMedia = wordCloudBuilder.getResult();
+
+            let postingPromises = this.startHandler.handlePosting(
+                status,
+                [
+                    imageMedia
+                ]
+            );
+            return Promise.all( postingPromises );
+        }
+        catch (e) {
+            return Promise.reject( e );
+        }
+    }
+}
+
+module.exports = SocialContentProvider;
