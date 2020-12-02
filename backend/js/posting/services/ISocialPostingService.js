394888bda68ec7b7bfaf79cf518a59018fa08477
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-12-02 17:11:30 +0100

    [Backend] Add ISocialPostingService interface and TwitterSocialPostingService to publish status content on social media

---
+++ backend/js/posting/services/ISocialPostingService.js
@@ -0,0 +1,15 @@
+class ISocialPostingService {
+    constructor() {
+
+    }
+
+    /**
+     *
+     * @param text {String}
+     * @param media {MediaInfo[]}
+     */
+    async addPublicPost( text, media ) {}
+
+}
+
+module.exports = ISocialPostingService;
---
+++ backend/js/posting/services/TwitterSocialPostingService.js
@@ -0,0 +1,83 @@
+const StatusCodes = require("http-status-codes").StatusCodes;
+const ISocialPostingService = require("./ISocialPostingService");
+
+const uploadClient = require("../../clients/1.1").getUserContextClient("upload");
+const postClient = require("../../clients/1.1").getUserContextClient("api");
+
+class TwitterSocialPostingService extends ISocialPostingService{
+    constructor() {
+        super();
+        this.client = {
+            uploading:  uploadClient,
+            posting: postClient
+        }
+    }
+
+
+    async addPublicPost(text, media) {
+
+        let mediaIds = [];
+        for (let i = 0; i < media.length; i++) {
+            let mediaInfo = media[ i ];
+            try {
+                let media_id = await this.uploadMedia(mediaInfo);
+                mediaIds.push( media_id );
+            }
+            catch (e) {
+                console.error( `[${this.constructor.name}]`, "Error uploading media", mediaInfo.mime, mediaInfo.metaInfo, "cause:\n", e );
+            }
+        }
+
+        try {
+            return await this.client.posting.post(
+                "/statuses/update",
+                {
+                    status: text,
+                    media_ids: mediaIds
+                }
+            )
+        }
+        catch (e) {
+            console.error( `[${this.constructor.name}]`, "Error posting status", text, "cause:\n", e );
+            return StatusCodes.BAD_GATEWAY
+        }
+
+
+    }
+
+    /**
+     *
+     * @param media {MediaInfo}
+     *
+     */
+    async uploadMedia( media) {
+        let mediaCategory = null;
+        if( media.mime.includes("image") ) {
+            if( media.mime.includes("gif") ) {
+                mediaCategory = "TweetGif";
+            }
+            else {
+                mediaCategory = "TweetImage";
+            }
+        }
+        else if( media.mime.includes("video") ) {
+            mediaCategory = "TweetVideo";
+        }
+        else {
+            console.warn( `[${this.constructor.name}]`, "Warning: attempting to upload unhandled media", media.mime );
+        }
+
+        let response = await this.client.uploading.post(
+            "media/upload",
+            {
+                media_data: media.data.toString("base64"),
+                media_type: media.mime,
+                media_category: mediaCategory
+            }
+        )
+
+        return response[ "media_id_string" ];
+    }
+}
+
+module.exports = TwitterSocialPostingService;
