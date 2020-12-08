6b2ddc4d601f5fcadea593668aec995b06473744
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-12-08 10:15:53 +0100

    [Backend] Add parentheses in filter's query generation to disambiguate

--- backend/js/filterConverter.js
+++ backend/js/filterConverter.js
@@ -4,21 +4,21 @@ function convertFilter(filter){
     if( filter ) {
         if( filter.keywords ) {
             if( filter.keywords.all && filter.keywords.all.length > 0 ) {
-                query += ` ${ filter.keywords.all.join( " " )}`;
+                query += ` (${ filter.keywords.all.join( " " )})`;
             }
             if( filter.keywords.exact && filter.keywords.exact.length > 0 ) {
-                query += ` "${filter.keywords.exact.join( " " )}"`;
+                query += ` ("${filter.keywords.exact.join( " " )}")`;
             }
             if( filter.keywords.any && filter.keywords.any.length > 0 ) {
-                query += ` ${ filter.keywords.any.join( " OR " )}`;
+                query += ` (${ filter.keywords.any.join( " OR " )})`;
             }
             if( filter.keywords.none && filter.keywords.none.length > 0 ) {
-                query += ` -${filter.keywords.none.join( " -" )}`;
+                query += ` (-${filter.keywords.none.join( " -" )})`;
             }
 
             if( filter.keywords.hashtags && filter.keywords.hashtags.length > 0 ) {
                 filter.keywords.hashtags.forEach( (item, key, array) => array[ key ] = item.replace(/#/g, "") )
-                query += ` #${filter.keywords.hashtags.join( " #" )}`;
+                query += ` (#${filter.keywords.hashtags.join( " #" )})`;
             }
 
         }
@@ -26,15 +26,15 @@ function convertFilter(filter){
         if( filter.accounts ) {
             if( filter.accounts.authors && filter.accounts.authors.length > 0 ) {
                 filter.accounts.authors.forEach( (item, key, array) => array[ key ] = item.replace(/@/g, "") )
-                query += ` from:${filter.accounts.authors.join( " from:")}`;
+                query += ` (from:${filter.accounts.authors.join( " from:")})`;
             }
             if( filter.accounts.replied && filter.accounts.replied.length > 0 ) {
                 filter.accounts.replied.forEach( (item, key, array) => array[ key ] = item.replace(/@/g, "") )
-                query += ` to:${filter.accounts.replied.join( " to:")}`;
+                query += ` (to:${filter.accounts.replied.join( " to:")})`;
             }
             if( filter.accounts.mentioning && filter.accounts.mentioning > 0 ) {
                 filter.accounts.mentioning.forEach( (item, key, array) => array[ key ] = item.replace(/@/g, "") )
-                query += ` @${filter.accounts.mentioning.join( " @")}`;
+                query += ` (@${filter.accounts.mentioning.join( " @")})`;
             }
         }
 
@@ -52,7 +52,7 @@ function convertFilter(filter){
                 query += ` lang:${filter.keywords.language}`;
             }
             if( filter.context.entities && filter.context.entities.length > 0 ) {
-                query += ` entity:${filter.context.entities.join( " entity:" )}`;
+                query += ` (entity:${filter.context.entities.join( " entity:" )})`;
             }
         }
 
@@ -61,7 +61,7 @@ function convertFilter(filter){
                 query += ` filter:links`;
             }
             if( filter.filters.linksValue ) {
-                query += ` url:${filter.filters.linksValue}`;
+                query += ` url:"${filter.filters.linksValue}"`;
             }
         }
 
