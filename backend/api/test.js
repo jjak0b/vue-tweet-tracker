fd74f78ffb69459cb1d581d51c99e294e93ad729
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-12-10 13:52:39 +0100

    [Backend] Remove obsolete data due to past merge mistake

--- backend/api/test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get( "/", (req, res) => {
-    res.write( "Hello World");
-    res.end();
-});
-
-module.exports = router;
